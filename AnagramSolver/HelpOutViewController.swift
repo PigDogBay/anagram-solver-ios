e92895289a7f147927bdd9107ed307d9cbe4a8fc
PigDogBay <pigdogbay@yahoo.co.uk>
2015-03-04 11:31:34 +0000

    implement rate and tell friend. added

--- AnagramSolver/HelpOutViewController.swift
+++ AnagramSolver/HelpOutViewController.swift
@@ -7,16 +7,35 @@
 //
 
 import UIKit
+import MessageUI
+import SwiftUtils
 
-class HelpOutViewController : PageContentController {
+class HelpOutViewController : PageContentController, MFMailComposeViewControllerDelegate {
 
     @IBAction func rateBtnPressed(sender: UIButton)
     {
-        NSLog("rate pressed")
+        //TO DO - insert own url
+        UIApplication.sharedApplication().openURL(NSURL(string: "itms-apps://itunes.apple.com/us/app/apple-store/id375380948?mt=8")!)
     }
-
+    
     @IBAction func tellFriendsBtnPressed(sender: UIButton)
     {
-        NSLog("tell friends pressed")
+        if !MFMailComposeViewController.canSendMail()
+        {
+            self.mpdbShowErrorAlert("No Email", msg: "This device is not configured for sending emails.")
+            return
+        }
+        var mailVC = MFMailComposeViewController()
+        mailVC.mailComposeDelegate = self
+        mailVC.setSubject("Anagram Solver")
+        mailVC.setMessageBody("Check out this free new app for iPhone, iPad and iPod touch - now available in the App Store<br/><br/><b>Anagram Solver</b> Ideal for crosswords, scrabble and word puzzle games<br/><br/><a href=\"http://google.co.uk\">Available on the App Store</a><br/><br/><a href=\"http://play.google.com/store/apps/details?id=com.pigdogbay.anagramsolver\">Available for Android on Google Play</a><br/><br/><br/>Thanks", isHTML: true)
+        presentViewController(mailVC, animated: true, completion: nil)
+    }
+    
+    // MARK:- MFMailComposeViewControllerDelegate
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!)
+    {
+        //dismiss on send
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
