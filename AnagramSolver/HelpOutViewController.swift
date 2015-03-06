aaabd012d33c554e086c117dbc6d662335ea24d9
PigDogBay <pigdogbay@yahoo.co.uk>
2015-03-06 10:15:37 +0000

    Added send feedback button

--- AnagramSolver/HelpOutViewController.swift
+++ AnagramSolver/HelpOutViewController.swift
@@ -17,6 +17,19 @@ class HelpOutViewController : PageContentController, MFMailComposeViewController
         //TO DO - insert own url
         UIApplication.sharedApplication().openURL(NSURL(string: Model.getAppUrl())!)
     }
+    @IBAction func feedbackBtnPressed(sender: UIButton)
+    {
+        if !MFMailComposeViewController.canSendMail()
+        {
+            self.mpdbShowErrorAlert("No Email", msg: "This device is not configured for sending emails.")
+            return
+        }
+        var mailVC = MFMailComposeViewController()
+        mailVC.mailComposeDelegate = self
+        mailVC.setSubject("Anagram Solver Feedback")
+        mailVC.setToRecipients(["pigdogbay@yahoo.co.uk"])
+        mailVC.setMessageBody("Your feedback is most welcome\n *Report Bugs\n *Suggest new features\n *Ask for help\n\n\nHi Mark,\n\n[Enter you message here]", isHTML: false)
+        presentViewController(mailVC, animated: true, completion: nil)    }
     
     @IBAction func tellFriendsBtnPressed(sender: UIButton)
     {
