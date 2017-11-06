863b2473d838cad6afb8c07015b68f5f36cd0bc0
PigDogBay <pigdogbay@yahoo.co.uk>
2017-11-06 12:04:57 +0000

    Wired up letters filter

--- AnagramSolver/FilterViewController.swift
+++ AnagramSolver/FilterViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var bannerView: UIView!
     @IBOutlet weak var tableView: UITableView!
@@ -19,10 +19,15 @@ class FilterViewController: UIViewController {
     @IBOutlet weak var bannerHeightConstraint: NSLayoutConstraint!
     
     fileprivate var model : Model!
+    fileprivate let numberCellId = "cellNumbersFilter"
+    fileprivate let letterCellId = "cellLettersFilter"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.model = Model.sharedInstance
+        tableView.dataSource = self
+        tableView.delegate = self
+
 
     }
 
@@ -32,6 +37,93 @@ class FilterViewController: UIViewController {
         } else {
             Ads.addAdView(container: bannerView)
         }
+    }
+    /*
+     Table layout
+     
+     Filter By Letters
+     [Contains |enter letters|]
+     [Excludes |enter letters|]
+     Letters can be in any order
+
+     Prefix / Suffix
+     [Prefix |enter letters|]
+     [Suffix |enter letters|]
+     Find words starting or ending with your specified letters.
+     
+     Filter By Word Size
+     Equal to             5>
+     Greater than   Not set>
+     Less than      Not set>
+     Press Search top right to perform a filter search
+
+     */
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 2
+        case 1:
+            return 2
+        case 2:
+            return 3
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Filter By Letters"
+        case 1:
+            return "Prefix / Suffix"
+        case 2:
+            return "Filter By Word Size"
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Letters can be in any order"
+        case 1:
+            return "Find words starting or ending with your specified letters."
+        case 2:
+            return "Press Search top right to perform a filter search"
+        default:
+            return ""
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath {
+        case [0,0]:
+            return cellForLettersFilter(indexPath, .contains)
+        case [0,1]:
+            return cellForLettersFilter(indexPath, .excludes)
+        case [1,0]:
+            return cellForLettersFilter(indexPath, .startsWith)
+        case [1,1]:
+            return cellForLettersFilter(indexPath, .endsWith)
+        default:
+            return tableView.dequeueReusableCell(withIdentifier: numberCellId, for: indexPath)
+        }
+    }
+    fileprivate func cellForLettersFilter(_ indexPath: IndexPath, _ filterType : LettersFilterTableViewCell.FilterType) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: letterCellId, for: indexPath) as! LettersFilterTableViewCell
+        cell.bind(filter: model.filter, filterType: filterType)
+        return cell
+    }
+    fileprivate func cellForNumbersFilter(_ indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: numberCellId, for: indexPath)
+        return cell
     }
 
 }
--- AnagramSolver/LettersFilterTableViewCell.swift
+++ AnagramSolver/LettersFilterTableViewCell.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class LettersFilterTableViewCell: UITableViewCell {
+class LettersFilterTableViewCell: UITableViewCell, UITextFieldDelegate {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var lettersTextView: UITextField!
+    
+    enum FilterType {
+        case startsWith, endsWith, contains, excludes
+    }
+    
+    fileprivate var filterType : FilterType?
+    fileprivate var filter : Filter?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,5 +30,47 @@ class LettersFilterTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    /*
+        Data binding is handled in this class as it seems the most convenient for now, similar to
+        Android's ViewHolder.
+        Ideally I would like to get rid of the enum and use reflection to pass in the field name
+    */
+    func bind(filter : Filter, filterType : FilterType){
+        self.filter = filter
+        self.filterType = filterType
+        lettersTextView.delegate = nil
 
+        switch filterType {
+        case .startsWith:
+            titleLabel.text = "Prefix"
+            lettersTextView.text = filter.startingWith
+        case .endsWith:
+            titleLabel.text = "Suffix"
+            lettersTextView.text = filter.endingWith
+        case .contains:
+            titleLabel.text = "Contains"
+            lettersTextView.text = filter.containing
+        case .excludes:
+            titleLabel.text = "Excludes"
+            lettersTextView.text = filter.excluding
+        }
+        lettersTextView.delegate = self
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let ft = filterType {
+            switch ft {
+            case .startsWith:
+                filter?.startingWith = lettersTextView.text!
+            case .endsWith:
+                filter?.endingWith = lettersTextView.text!
+            case .contains:
+                filter?.containing = lettersTextView.text!
+            case .excludes:
+                filter?.excluding = lettersTextView.text!
+            }
+        }
+
+    }
 }
