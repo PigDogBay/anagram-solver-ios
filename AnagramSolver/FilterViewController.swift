a9a1a1139f7f59efcc56f5d873fc0cc1ee0ffc89
PigDogBay <pigdogbay@yahoo.co.uk>
2017-11-06 15:21:25 +0000

    Wired up number filters

--- AnagramSolver/FilterViewController.swift
+++ AnagramSolver/FilterViewController.swift
@@ -18,6 +18,27 @@ class FilterViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet weak var bannerHeightConstraint: NSLayoutConstraint!
     
+
+    @IBAction func unwindWithSelectedListItem(segue:UIStoryboardSegue) {
+        if let vc = segue.source as? ListViewController {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                if let value = vc.selectedIndex {
+                    switch indexPath {
+                    case [2,0]:
+                        model.filter.equalTo = value
+                    case [2,1]:
+                        model.filter.biggerThan = value
+                    case [2,2]:
+                        model.filter.lessThan = value
+                    default:
+                        break
+                    }
+                    tableView.reloadData()
+                }
+            }
+        }
+    }
+    
     fileprivate var model : Model!
     fileprivate let numberCellId = "cellNumbersFilter"
     fileprivate let letterCellId = "cellLettersFilter"
@@ -92,9 +113,9 @@ class FilterViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         switch section {
         case 0:
-            return "Letters can be in any order"
+            return "Letters can be in any order here"
         case 1:
-            return "Find words starting or ending with your specified letters."
+            return "Find words starting or ending with your specified letters"
         case 2:
             return "Press Search top right to perform a filter search"
         default:
@@ -112,6 +133,12 @@ class FilterViewController: UIViewController, UITableViewDataSource, UITableView
             return cellForLettersFilter(indexPath, .startsWith)
         case [1,1]:
             return cellForLettersFilter(indexPath, .endsWith)
+        case [2,0]:
+            return cellForNumbersFilter(indexPath, model.filter.equalTo, "Equal To")
+        case [2,1]:
+            return cellForNumbersFilter(indexPath, model.filter.biggerThan, "Greater Than")
+        case [2,2]:
+            return cellForNumbersFilter(indexPath, model.filter.lessThan, "Less Than")
         default:
             return tableView.dequeueReusableCell(withIdentifier: numberCellId, for: indexPath)
         }
@@ -121,9 +148,44 @@ class FilterViewController: UIViewController, UITableViewDataSource, UITableView
         cell.bind(filter: model.filter, filterType: filterType)
         return cell
     }
-    fileprivate func cellForNumbersFilter(_ indexPath: IndexPath) -> UITableViewCell {
+    fileprivate func cellForNumbersFilter(_ indexPath: IndexPath,_ value : Int,_ title : String) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: numberCellId, for: indexPath)
+        cell.textLabel?.text = title;
+        if value == 0 {
+            cell.detailTextLabel?.text = "Not set"
+        } else {
+            cell.detailTextLabel?.text = "\(value) letters"
+        }
         return cell
     }
 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier! {
+        case "segueShowNumberList":
+            if let vc = segue.destination as? ListViewController {
+                vc.listItems = ["No Filter","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
+                vc.selectedIndex = 0
+                if let selectedCell = sender as? UITableViewCell{
+                    if let indexPath = tableView.indexPath(for: selectedCell) {
+                        switch indexPath {
+                        case [2,0]:
+                            vc.title = "Equal To"
+                            vc.selectedIndex = model.filter.equalTo
+                        case [2,1]:
+                            vc.title = "Greater Than"
+                            vc.selectedIndex = model.filter.biggerThan
+                        case [2,2]:
+                            vc.title = "Less Than"
+                            vc.selectedIndex = model.filter.lessThan
+                        default:
+                            break
+                        }
+                    }
+                }
+            }
+        default:
+            break
+        }
+    }
 }
