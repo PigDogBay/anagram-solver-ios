c478ce2c7fec0b918f90551740a4e419b8f895e8
PigDogBay <pigdogbay@yahoo.co.uk>
2017-11-07 18:15:01 +0000

    Wired up collection cell UI to code

---
+++ AnagramSolver/TipCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TipCollectionViewCell.swift
+//  AnagramSolver
+//
+//  Created by Mark Bailey on 07/11/2017.
+//  Copyright © 2017 MPD Bailey Technology. All rights reserved.
+//
+
+import UIKit
+
+class TipCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+       
+    @IBAction func showMeClicked(_ sender: UIButton) {
+    }
+}
