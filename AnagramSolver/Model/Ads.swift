3212d56e6f8585ab33d8c268f257650cd5f845dd
Mark Bailey <markpdbailey@gmail.com>
2021-07-14 11:17:54 +0100

    Updated test id for iPod

--- AnagramSolver/Model/Ads.swift
+++ AnagramSolver/Model/Ads.swift
@@ -49,7 +49,7 @@ class Ads
         requestConfiguration.testDeviceIdentifiers = [
             (kGADSimulatorID as! String),
             "0200fa505875358d7e2d07bc993bd27f",//iPad
-            "0ead52ca5eafcbe25f3b929cda2cdbf1"//iPod Touch
+            "a4b042150b6cace14cc182d6bf254d09"//iPod Touch
            ]
         #endif
         GADMobileAds.sharedInstance().start(completionHandler: nil)
