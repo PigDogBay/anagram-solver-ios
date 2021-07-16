9d62da87c55d714c018fd1682cdc7372e72cf9f1
Mark Bailey <markpdbailey@gmail.com>
2021-07-16 13:10:32 +0100

    TipCard view

---
+++ AnagramSolver/TipCard.swift
@@ -0,0 +1,91 @@
+//
+//  TipCard.swift
+//  AnagramSolver
+//
+//  Created by Mark Bailey on 16/07/2021.
+//  Copyright © 2021 MPD Bailey Technology. All rights reserved.
+//
+
+import SwiftUI
+
+struct TipCard: View {
+    let tip : Tip
+    
+    
+    private func buttonPressed(){
+        
+    }
+    
+    private var title : some View {
+        HStack {
+            Image("lightbulb_on")
+                .padding(8)
+            Spacer()
+            Text(tip.title).font(.title)
+            Spacer()
+            Spacer()
+        }
+    }
+    
+    private var description : some View {
+        VStack(alignment: .leading, spacing: 5){
+            Text(tip.subtitle)
+            showMeExample
+            Text(tip.example)
+                .foregroundColor(Color("exampleResult"))
+        }
+    }
+    private var showMeExample : some View {
+        HStack(spacing: 0) {
+            Text("Try ")
+            Text(tip.showMe)
+                .foregroundColor(Color("exampleQuery"))
+            Text(" to find:")
+        }
+    }
+    
+    private var buttons : some View {
+        HStack(){
+            Button(action: buttonPressed){
+                Text("MORE INFO")
+                    .modifier(TipButtonMod())
+            }.buttonStyle(BorderlessButtonStyle())
+            Spacer()
+            Button(action: buttonPressed){
+                Text("SHOW ME")
+                    .modifier(TipButtonMod())
+            }.buttonStyle(BorderlessButtonStyle())
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            title
+                .padding(.top,16)
+            description
+                .padding(.top,2)
+            buttons
+                .padding(16)
+        }
+    }
+}
+
+struct TipButtonMod : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.footnote)
+            .foregroundColor(Color("materialButton"))
+    }
+}
+
+struct TipCard_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            TipCard(tip: tipsData[0])
+                .preferredColorScheme(.dark)
+            TipCard(tip: tipsData[1])
+            TipCard(tip: tipsData[2])
+        }
+        .previewLayout(.fixed(width: 300, height: 210))
+    }
+}
