119ec0bf71fee09d12021f6656e2e8ba72f600fb
Mark Bailey <markpdbailey@gmail.com>
2021-07-16 16:23:01 +0100

    CardTips initial checkin

---
+++ AnagramSolver/CardTips.swift
@@ -0,0 +1,34 @@
+//
+//  CardTips.swift
+//  AnagramSolver
+//
+//  Created by Mark Bailey on 16/07/2021.
+//  Copyright © 2021 MPD Bailey Technology. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardTips: View {
+    @ObservedObject var coordinator : Coordinator
+    
+    var body: some View {
+        List {
+            ForEach(tipsData) { tip in
+                TipCard(tip: tip)
+                    .background(Color.white)
+                    .cornerRadius(5.0)
+                    .shadow(radius: 2)
+                    .listRowBackground(Color("tipsBackground"))
+            }
+        }
+        .gesture(DragGesture().onChanged { _ in
+            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
+        })
+    }
+}
+
+struct CardTips_Previews: PreviewProvider {
+    static var previews: some View {
+        CardTips(coordinator: Coordinator(rootVC: RootViewController()))
+    }
+}
