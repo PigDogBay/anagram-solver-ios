45f8ca457399f3fc2d977deb180323c46cf04e8b
Mark Bailey <markpdbailey@gmail.com>
2023-09-07 11:09:10 +0100

    Fixed bug where navigation links where creating empty space when Accessibility->Button Shapes was enabled

--- AnagramSolver/RootView.swift
+++ AnagramSolver/RootView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 
 struct RootView: View {
     @ObservedObject var coordinator = Coordinator.sharedInstance
+
+
+    /*
+     There is a bug where navigation links appear as empty boxes when
+     Settings->Accessibility->Display->Button Shapes enabled
+     Setting the button style to plain removes the empty shapes and white space at the bottom of the screen
+     https://stackoverflow.com/questions/70223291/eliminate-navigationlink-button-shape-in-accessibility-mode
+    */
     var body: some View {
         if coordinator.showCards {
             CardTips()
@@ -21,32 +29,32 @@ struct RootView: View {
                        tag: coordinator.SHOW_ABOUT,
                        selection: $coordinator.selection){
             EmptyView()
-        }
+        }.buttonStyle(.plain)
         NavigationLink(destination: DefinitionHelpView(),
                        tag: coordinator.SHOW_DEFINITION_HELP,
                        selection: $coordinator.selection){
             EmptyView()
-        }
+        }.buttonStyle(.plain)
         NavigationLink(destination: FilterHelpView(),
                        tag: coordinator.SHOW_FILTER_HELP,
                        selection: $coordinator.selection){
             EmptyView()
-        }
+        }.buttonStyle(.plain)
         NavigationLink(destination: HelpView(viewModel: HelpViewModel(tip: coordinator.tip)),
                        tag: coordinator.SHOW_HELP,
                        selection: $coordinator.selection){
             EmptyView()
-        }
+        }.buttonStyle(.plain)
         NavigationLink(destination: SettingsHelpView(),
                        tag: coordinator.SHOW_SETTINGS_HELP,
                        selection: $coordinator.selection){
             EmptyView()
-        }
+        }.buttonStyle(.plain)
         NavigationLink(destination: SettingsView(),
                        tag: coordinator.SHOW_SETTINGS,
                        selection: $coordinator.selection){
             EmptyView()
-        }
+        }.buttonStyle(.plain)
     }
 }
 
