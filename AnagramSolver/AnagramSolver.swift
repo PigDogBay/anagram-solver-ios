d5f4c128b9eedf3743e6d4fdfb84bca41ba6d3bc
Mark Bailey <markpdbailey@gmail.com>
2026-03-10 13:50:47 +0000

    Start of UIKit to SwiftUI migration. Created new App entry point that displays simple hello world

---
+++ AnagramSolver/AnagramSolver.swift
@@ -0,0 +1,32 @@
+//
+//  AnagramSolver.swift
+//  AnagramSolver
+//
+//  Created by Mark Bailey on 10/03/2026.
+//  Copyright © 2026 MPD Bailey Technology. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AnagramSolverApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
