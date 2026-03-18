c9ff6301df679090338aa416b73219eee59d856c
Mark Bailey <markpdbailey@gmail.com>
2026-03-18 13:35:42 +0000

    NMA AutoTest now uses Task and @Observable

--- AnagramSolver/NewModelArmy/NMAAutoTest.swift
+++ AnagramSolver/NewModelArmy/NMAAutoTest.swift
@@ -7,27 +7,32 @@
 //
 
 import Foundation
-import Combine
 import SwiftUtils
 
 @MainActor
 class NMAAutoTest {
     private let model : NMAModel
     private let appVM : AppViewModel
-    private var disposables = Set<AnyCancellable>()
     private let randomQuery = RandomQuery()
     
     init(model: NMAModel, appVM: AppViewModel) {
         self.model = model
         self.appVM = appVM
-
-        model.$appState
-            .sink(receiveValue:{[weak self] appState in self?.onAppState(newState: appState)})
-            .store(in: &disposables)
-
+        
+        model.onStateChange = { [weak self] newState in
+            self?.stateDidChange(newState)
+        }
+        //Start the first search
+        stateDidChange(.ready)
     }
     
-    private func onAppState(newState : AppStates){
+    private func stateDidChange(_ newState : AppStates) {
+        Task {
+            try await update(newState)
+        }
+    }
+    
+    private func update(_ newState : AppStates) async throws {
         switch newState {
         case .uninitialized:
             print("AutoTest: uninitialized")
@@ -36,21 +41,17 @@ class NMAAutoTest {
             print("AutoTest: Loading")
         case .ready:
             print("AutoTest: Ready")
-            Task {
-                self.appVM.searchBarVM.query = self.randomQuery.query()
-                try await Task.sleep(for: .seconds(2))
-                self.appVM.search()
-            }
+            self.appVM.searchBarVM.query = self.randomQuery.query()
+            try await Task.sleep(for: .seconds(2))
+            self.appVM.search()
         case .searching:
             print("AutoTest: Searching")
             break
         case .finished:
             print("AutoTest: Finished")
-            Task {
-                try await Task.sleep(for: .seconds(5))
-                self.appVM.goBack()
-                self.model.appState = .ready
-            }
+            try await Task.sleep(for: .seconds(5))
+            self.appVM.goBack()
+            self.model.appState = .ready
         case .error:
             print("AutoTest: App Error Detected")
         }
--- AnagramSolver/NewModelArmy/NMAModel.swift
+++ AnagramSolver/NewModelArmy/NMAModel.swift
@@ -13,7 +13,23 @@ import SwiftUtils
     Data that needs to be shared between various view-models
     AppState needs to be tracked by AutoTest
  */
-class NMAModel : ObservableObject {
+@Observable
+class NMAModel {
+    var appState: AppStates = .uninitialized {
+        didSet { onStateChange?(appState) }
+    }
+    var onStateChange: ((AppStates) -> Void)?
+
+    @ObservationIgnored let engine = WordEngine()
+    @ObservationIgnored lazy private(set) var searchHistoryModel =
+    {
+        SearchHistoryModel(persistence: SearchHistoryUserDefaults(),
+                           isSearchHistoryEnabled: Settings().isSearchHistoryEnabled)
+    }()
+
+}
+
+class NMAModelX : ObservableObject {
     @Published var appState = AppStates.uninitialized
     let engine = WordEngine()
     lazy private(set) var searchHistoryModel =
